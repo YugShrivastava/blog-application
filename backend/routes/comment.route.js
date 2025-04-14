3ccb93d22f78319d1c7cc98d55ee6764f710afaf
Yug <shrivastavayug04@gmail.com>
2025-04-14 14:48:31 +0530

    backend deleted

--- backend/routes/comment.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addComment, getAllComments } = require('../controllers/comment.controller');
-const router = express.Router();
-
-router.post('/add', addComment);
-router.get('/', getAllComments);
-
-module.exports = router;
